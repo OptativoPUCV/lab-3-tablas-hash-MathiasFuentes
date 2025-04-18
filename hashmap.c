aee4b619bd26d9b01e36f8bfc929ba5c53ba4849
Your Name <you@example.com>
2025-04-18 14:14:25 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -103,7 +103,7 @@ e - Inserte los elementos del arreglo old_buckets en el mapa (use la función in
 void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
 
-    if (map == NULL || map->buckets == NULL) return NULL;
+    if (map == NULL || map->buckets == NULL) return;
     long old_capacity = map->capacity;
     Pair** old_buckets = map->buckets;
 
