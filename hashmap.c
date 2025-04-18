525fba34d616e3973a1b75895ca61d9e31706df5
Your Name <you@example.com>
2025-04-18 13:18:25 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -11,9 +11,9 @@ int enlarge_called=0;
 
 struct HashMap {
     Pair ** buckets;
-    long size; //cantidad de datos/pairs en la tabla
-    long capacity; //capacidad de la tabla
-    long current; //indice del ultimo dato accedido
+    long size;          //cantidad de datos/pairs en la tabla
+    long capacity;      //capacidad de la tabla
+    long current;       //indice del ultimo dato accedido
 };
 
 Pair * createPair( char * key,  void * value) {
@@ -38,10 +38,44 @@ int is_equal(void* key1, void* key2){
     return 0;
 }
 
+/*
+2.- Implemente la función void insertMap(HashMap * map, char * key, void * value). 
+Esta función inserta un nuevo dato (key,value) en el mapa y actualiza el índice current a esa posición. 
+
+Recuerde que para insertar un par (clave,valor) debe:
+a - [LISTO] Aplicar la función hash a la clave para obtener la posición donde debería insertar el nuevo par
+
+b - [LISTO] Si la casilla se encuentra ocupada, avance hasta una casilla disponible (método de resolución de colisiones). Una casilla disponible es una casilla nula, 
+    pero también una que tenga un par inválido (key==NULL).
+
+c - [LISTO] Ingrese el par en la casilla que encontró.
+
+[LISTO] No inserte claves repetidas. Recuerde que el arreglo es circular. Recuerde actualizar la variable size.
+*/
 
 void insertMap(HashMap * map, char * key, void * value) {
+    long pos = hash(key, map->capacity);
+    long originalPos = pos;
 
+    // Si la posición pos es efectivamente NULL, este while no hará nada
+    while (map->buckets[pos] != NULL) {
+        // No se insertan claves duplicadas.
+        if (map->buckets[pos]->key != NULL && is_equal(map->buckets[pos]->key, key)) {
+            return;
+        }
 
+        pos = (pos + 1) % map->capacity;
+        if (pos == originalPos) return;
+    }
+
+    // Insertar en el mapa
+    Pair* newPair = (Pair*)malloc(sizeof(Pair));
+    newPair->key = strdup(key);
+    newPair->value = value;
+
+    map->buckets[pos] = newPair;
+    map->size += 1;
+    map->current = pos;
 }
 
 void enlarge(HashMap * map) {
@@ -50,10 +84,34 @@ void enlarge(HashMap * map) {
 
 }
 
+/*
+Esta función crea una variable de tipo HashMap, inicializa el arreglo de buckets con casillas nulas, 
+inicializa el resto de variables y retorna el mapa. Inicialice el índice current a -1.
+*/
 
 HashMap * createMap(long capacity) {
+    // 1. Reservar memoria para el mapa.
+    HashMap* map = (HashMap*)malloc(sizeof(HashMap));
+    if (map == NULL) return NULL;
+    
+    // 1.2. Inicializar las variables del mapa.
+    map->capacity = capacity;
+    map->current = -1;
+    map->size = 0;
+    
+    // 2. Reservar memoria para el arreglo de buckets.
+    map->buckets = (Pair**)malloc(sizeof(Pair*) * capacity);
+    if (map->buckets == NULL){
+        free(map);
+        return NULL;
+    }
+    
+    // 2.1. Inicializar el arreglo en NULL.
+    for (int i=0; i < capacity ; i++){
+        map->buckets[i] = NULL;
+    }
 
-    return NULL;
+    return map;
 }
 
 void eraseMap(HashMap * map,  char * key) {    
@@ -61,9 +119,34 @@ void eraseMap(HashMap * map,  char * key) {
 
 }
 
+/*
+3.- Implemente la función Pair * searchMap(HashMap * map, char * key), la cual retorna el Pair asociado a la clave ingresada. 
+
+Recuerde que para buscar el par debe:
+a - [LISTO] Usar la función hash para obtener la posición donde puede encontrarse el par con la clave
+
+b - [LISTO] Si la clave no se encuentra avance hasta encontrarla (método de resolución de colisiones)
+
+c - [LISTO] Si llega a una casilla nula, retorne NULL inmediatamente (no siga avanzando, la clave no está)
+
+Recuerde actualizar el índice current a la posición encontrada. Recuerde que el arreglo es circular.
+*/
+
 Pair * searchMap(HashMap * map,  char * key) {   
+    long pos = hash(key, map->capacity);
+    long originalPos = pos;
 
+    while (map->buckets[pos] != NULL){
+        if (map->buckets[pos]->key != NULL && is_equal(map->buckets[pos]->key, key)){
+            map->current = pos;
+            return map->buckets[pos];
+        }
+        pos = (pos + 1) % map->capacity;
 
+        if (pos == originalPos) break;
+    }
+    
+    // Se topó una key=NULL o no se encontró la key.
     return NULL;
 }
 
