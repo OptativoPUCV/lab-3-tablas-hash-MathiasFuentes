9a057d5c4047a331a486a20c3de38d107b529453
Your Name <you@example.com>
2025-04-18 13:38:35 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -114,9 +114,12 @@ HashMap * createMap(long capacity) {
     return map;
 }
 
-void eraseMap(HashMap * map,  char * key) {    
+void eraseMap(HashMap * map, char * key) {
+    Pair * pair = searchMap(map, key);
+    if (pair == NULL) return;
 
-
+    pair->key = NULL;
+    map->size -= 1;
 }
 
 /*
